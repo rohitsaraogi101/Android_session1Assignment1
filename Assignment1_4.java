485aa9097eb7050f8fe3a1afec17b5a3c5cd123e
rohitsaraogi101 <rohitsaraogi101@gmail.com>
2016-12-18 00:58:29 +0530

    Create Assignment1_4.java

---
+++ Assignment1_4.java
@@ -0,0 +1,12 @@
+public class Assignment1_4 {
+	public static void main(String args[])
+	{
+		char x;
+		
+		for(int i=65;i<=90;i++)
+		{
+			x = (char)i;
+			System.out.println(i+"-"+x);
+		}
+	}
+}
