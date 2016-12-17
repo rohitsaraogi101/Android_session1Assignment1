faf96f2321822642130e4b2bf2ec6c0c4154b2d0
rohitsaraogi101 <rohitsaraogi101@gmail.com>
2016-12-18 00:54:14 +0530

    Create Assignment1_2.java

---
+++ Assignment1_2.java
@@ -0,0 +1,18 @@
+public class Assignment1_2 {
+	public static void main(String[] args) {
+		
+   int a = 20;
+	 int b = 10;
+		 System.out.println("The initial value of a is =  "+a);
+		 System.out.println("The initial value of b is = "+b);
+		int b = a-- - --a;
+		int c = a--;
+		int d = a>>2;
+		int e = a&b;
+		
+		 System.out.println("the final value of b is = "+b);
+		 System.out.println("the value of c is = "+c);
+		 System.out.println("The value of d is = "+d);
+		 System.out.println("The value of e is = "+e);
+	}
+}
